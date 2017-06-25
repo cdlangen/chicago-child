630bd18a1e7721e47c526f839631aad378ca85c0
carolynlangen <carolyn@thehyve.nl>
2017-06-25 18:17:31 +0200

    Add Academicons icons (ResearchGate and Google Scholar)

--- functions.php
+++ functions.php
@@ -1,15 +1,31 @@
 <?php
-/**
- * Child Theme functions and definitions
- *
- */
 
 
-/**
- * Loading Parent theme stylesheet
- *
+/* borrowed from https://www.sitepoint.com/using-font-awesome-with-wordpress/
+ * I added the second wp_enqueue_style line to include Academicons
+ * This code enqueues our external font awesome stylesheet 
+ * 
  */
-add_action( 'wp_enqueue_scripts', 'catchbox_child_enqueue_styles' );
-function catchbox_child_enqueue_styles() {
-    wp_enqueue_style( 'catchbox-parent-style', get_template_directory_uri() . '/style.css' );
+function enqueue_our_required_stylesheets(){
+    wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/adc2e6740b.js'); 
+    wp_enqueue_style('academicons', get_stylesheet_directory_uri() . '/css/academicons.css'); 
 }
+add_action('wp_enqueue_scripts','enqueue_our_required_stylesheets');
+
+/**
+ * Loading Parent and child theme stylesheets
+ *
+ */
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'chicago-style';
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+?>
